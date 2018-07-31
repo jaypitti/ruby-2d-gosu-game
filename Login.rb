4c7e3d47d1eb1a2081dd5665ea24cd27e1d6a8dc
Jandrei Pitti <jandreipitti@Jandreis-MacBook-Pro.local>
2018-07-31 15:49:39 -0600

    ADDED HOVER EFFECT TO LOGIN BUTTON

--- Login.rb
+++ Login.rb
@@ -9,31 +9,31 @@ class TextField < Gosu::TextInput
   WIDTH = 350
   LENGTH_LIMIT = 20
   PADDING = 5
-  
+
   INACTIVE_COLOR  = 0xcc_666666
   ACTIVE_COLOR    = 0xcc_ff6666
   SELECTION_COLOR = 0xcc_0000ff
   CARET_COLOR     = 0xff_ffffff
-  
+
   attr_reader :x, :y
-  
+
   def initialize(window, x, y)
     # It's important to call the inherited constructor.
     super()
-    
+
     @window, @x, @y = window, x, y
-    
+
     # Start with a self-explanatory text in each field.
     self.text = "Click to edit"
   end
-  
+
   # In this example, we use the filter method to prevent the user from entering a text that exceeds
   # the length limit. However, you can also use this to blacklist certain characters, etc.
   def filter new_text
     allowed_length = [LENGTH_LIMIT - text.length, 0].max
     new_text[0, allowed_length]
   end
-  
+
   def draw(z)
     # Change the background colour if this is the currently selected text field.
     if @window.text_input == self
@@ -42,12 +42,12 @@ class TextField < Gosu::TextInput
       color = INACTIVE_COLOR
     end
     Gosu.draw_rect x - PADDING, y - PADDING, WIDTH + 2 * PADDING, height + 2 * PADDING, color, z
-    
+
     # Calculate the position of the caret and the selection start.
     pos_x = x + FONT.text_width(self.text[0...self.caret_pos])
     sel_x = x + FONT.text_width(self.text[0...self.selection_start])
     sel_w = pos_x - sel_x
-    
+
     # Draw the selection background, if any. (If not, sel_x and pos_x will be
     # the same value, making this a no-op call.)
     Gosu.draw_rect sel_x, y, sel_w, height, SELECTION_COLOR, z
@@ -60,7 +60,7 @@ class TextField < Gosu::TextInput
     # Finally, draw the text itself!
     FONT.draw self.text, x, y, z
   end
-  
+
   def height
     FONT.height
   end
@@ -68,7 +68,7 @@ class TextField < Gosu::TextInput
   # Hit-test for selecting a text field with the mouse.
   def under_mouse?
     @window.mouse_x > x - PADDING and @window.mouse_x < x + WIDTH + PADDING and
-    @window.mouse_y > y - PADDING and @window.mouse_y < y + height + PADDING
+      @window.mouse_y > y - PADDING and @window.mouse_y < y + height + PADDING
   end  
   # Tries to move the caret to the position specifies by mouse_x
   def move_caret_to_mouse
@@ -88,33 +88,42 @@ class Login < (Example rescue Gosu::Window)
   def initialize
     super 640, 480
     self.caption = "LOGIN"
-    
-    
+
+
     text = "RUBY 2D"
 
     # Remove all leading spaces so the text is left-aligned
     text.gsub! /^ +/, ''
-    
+
     @text = Gosu::Image.from_text text, 20, :width => 540
-        
+
     # Set up an array of three text fields.
     @text_fields = Array.new(2) { |index| TextField.new(self, 50, 300 + index * 50) }
     @text_fields[0].text = "User Name"
     @text_fields[1].text = "Password"
   end
-  
+
   def needs_cursor?
     true
   end
 
   def draw
+    color = 0xcc_C0C0C0
+
+    if  self.mouse_x > 450 and self.mouse_x < 450 + 125 and
+        self.mouse_y > 310 and self.mouse_y < 310 + 50
+      color = 0xcc_00FFFF
+    else
+      color = 0xcc_C0C0C0
+    end
     @text.draw 50, 50, 0
     @text_fields.each { |tf| tf.draw(0) }
     @test = Gosu::Image.from_text "LOGIN", 30, :width => 125
-    @login_button = Gosu.draw_rect 450, 300, 125, 50, 0xcc_C0C0C0, 0
+    @login_button = Gosu.draw_rect 450, 300, 125, 50, color, 0
     @test.draw 470, 310, 1
+
   end
-  
+
   def button_down(id)
     if id == Gosu::KB_TAB
       # Tab key will not be 'eaten' by text fields; use for switching through
@@ -129,30 +138,30 @@ class Login < (Example rescue Gosu::Window)
         close
       end
     elsif id == Gosu::MS_LEFT
-      
+
       button = ""
       # Mouse click: Select text field based on mouse position.
       self.text_input = @text_fields.find { |tf| tf.under_mouse? }
       # Also move caret to clicked position
-      
+
       if  self.mouse_x > 450 and self.mouse_x < 450 + 125 and
           self.mouse_y > 310 and self.mouse_y < 310 + 50 
-          @game = Game.new @text_fields[0].text, "", "1234"
-          @game.show
-          close
+        @game = Game.new @text_fields[0].text, "", "1234"
+        @game.show
+        close
       end
       self.text_input.move_caret_to_mouse unless self.text_input.nil?
     elsif id == Gosu::KB_RETURN
-      
+
       @game = Game.new @text_fields[0].text, "", "1234"
       @game.show
-      
-#      uri = URI('https://example.com/some_path?query=string')
- #     response = Net::HTTP.start(uri.host, uri.port) do |http|
-  #    request = Net::HTTP::Get.new uri.request_uri
-   #   http.request request
+
+      #      uri = URI('https://example.com/some_path?query=string')
+      #     response = Net::HTTP.start(uri.host, uri.port) do |http|
+      #    request = Net::HTTP::Get.new uri.request_uri
+      #   http.request request
       close
-#      end
+      #      end
     else
       super
     end
